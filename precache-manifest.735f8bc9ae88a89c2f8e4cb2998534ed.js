dbe017b8123992922ad49bc8535e6559da2cd067
Kishan Kumar <kishan.kumar@intimetec.com>
2019-08-04 18:58:56 +0530

    Updates

---
+++ precache-manifest.735f8bc9ae88a89c2f8e4cb2998534ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d6a04d50a8a6cb40f5db620d4fa814",
+    "url": "/NRKishanKumar/reactified.tictactoe.io.git/index.html"
+  },
+  {
+    "revision": "b8537dceae5ad2fafb29",
+    "url": "/NRKishanKumar/reactified.tictactoe.io.git/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/NRKishanKumar/reactified.tictactoe.io.git/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "b8537dceae5ad2fafb29",
+    "url": "/NRKishanKumar/reactified.tictactoe.io.git/static/js/main.edfba8b8.chunk.js"
+  },
+  {
+    "revision": "3870901f1e162ca54f44",
+    "url": "/NRKishanKumar/reactified.tictactoe.io.git/static/js/runtime~main.d11041d0.js"
+  }
+]);
